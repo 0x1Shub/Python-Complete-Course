3fdfa7e9abd5dc1cd3f46f96da0fa43d24e6da41
0x1Shub <shubhamyeram13@gmail.com>
2025-03-24 14:28:40 +0530

    Strings

---
+++ 05-Strings/01-Strings.py
@@ -0,0 +1,23 @@
+course = "Python for Beginneers"
+
+print(course)
+
+# Converts all characters to uppercase. It not modify our string it return new strings
+print(course.upper())
+
+# Converts all characters to lowercase
+print(course.lower())
+
+# Find that Character
+print(course.find('y'))
+print(course.find('Y'))
+print(course.find("for"))
+
+# Replace the character or word. It not modify our string it return new strings
+print(course.replace('for', '4'))
+
+# use in method to find the word 
+print('for' in course)
+
+print(course)
+
