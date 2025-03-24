2d5ab2b4ea1abc4154e1f09934857bf659323abf
0x1Shub <shubhamyeram13@gmail.com>
2025-03-24 15:46:44 +0530

    Lists

---
+++ 09-Lists/01_Lists.py
@@ -0,0 +1,11 @@
+names = ["Shubham", "John", "Bob", "Mosh", "Sam"]
+
+print(names)
+print(names[0])
+print(names[-1]) # Prints the last element
+print(names[-2]) # Prints second last element
+
+names[0] = "Shub"
+print(names)
+
+print(names[0:3]) # print from 0 to 3 index but 3 is excluded
---
+++ 09-Lists/02_List_Methods.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6) # It appends/adds number in list
+
+numbers.insert(0, -1)
+print(numbers)
+
+numbers.insert(5, -10)
+print(numbers)
+
+# Find the value inside the list
+print(10 in numbers)
+print(len(numbers))
