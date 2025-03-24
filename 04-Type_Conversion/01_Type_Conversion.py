cba593afc05615a29c7a93ebd9a9849c7d8f76aa
0x1Shub <shubhamyeram13@gmail.com>
2025-03-24 14:04:10 +0530

    Type Conversion

---
+++ 04-Type_Conversion/01_Type_Conversion.py
@@ -0,0 +1,11 @@
+birth_year = input("Enter Your Birth Year: ")
+age = 2025 - int(birth_year)
+print("Your age is:", age)
+
+price = input("Enter a price of object: ")
+profit = 300.00 - float(price)
+print("Your profit is: ", profit)
+
+# Some similar type coversion fuction are
+# str()
+# bool()
---
+++ 04-Type_Conversion/exercise.py
@@ -0,0 +1,7 @@
+# write a basic calculator program taking two numbers whole number and decimal number and program will give sum of two number.
+
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+
+sum = float(first_number) + float(second_number)
+print("Sum is: " + str(sum))
